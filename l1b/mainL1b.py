e19fb557c433dbe3702b75329343852e33004890
diegotiradohernandez <diego.ivan.tirado@estudiantat.upc.edu>
2023-09-21 19:30:06 +0200

    Commit 2nd Class - L1B implemented

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,10 +4,11 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\input"
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\myoutputs"
+auxdir = r'C:\Users\diego\PycharmProjects\pythonProjectNewGIT\auxiliary'
+indir = r"C:\\Users\diego\\PycharmProjects\\EODP_TER_2021\\EODP-TS-L1B\\input"
+outdir = r"C:\\Users\\diego\\PycharmProjects\\EODP_TER_2021\\EODP-TS-L1B\\myoutputs"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
 myL1b.processModule()
+
